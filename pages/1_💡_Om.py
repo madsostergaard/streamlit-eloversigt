0b09522e6b79729c1176ac3bce923609eb6fd9ca
Mads Oestergaard <mads.oestergaard@wsa.com>
2022-10-19 21:18:53 +0200

    💡 write a description text

--- pages/1_💡_Om.py
+++ pages/1_💡_Om.py
@@ -5,10 +5,23 @@ st.set_page_config(page_title="Om", page_icon="💡")
 st.title("💡 Om")
 st.markdown(
     """
+## Streamlit-eloversigt
+Værktøj til at vise elforbrug og -priser. 
+Det er også muligt at hente data fra AQUAREA Smart-Cloud hvis man har en Panasonic varmepumpe, som jeg har. 
+
 ## Opsætning
-Sådan henter du et _refresh token_ fra [eloverblik](https://eloverblik.dk):
-1. Gå til eloverblik.dk
-2. ...
+Du har brug for et token fra [eloverblik](https://eloverblik.dk) for at kunne hente dit eldata. 
+Se altid [nuværende vejledninger her](https://energinet.dk/Energidata/DataHub/Eloverblik).
+Et refreshtoken er gyldig i et år, men du kan til enhver tid deaktivere eller slette dit token
+
+Sådan danner du et token:
+1. Gå til eloverblik.dk.
+2. Log ind med nem-id (eller MitID når det bliver implementeret). Bemærk: du skal logge ind som den, der har oprettet jeres elaftale.
+3. Klik på profilikonet i højre hjørne og klik derefter på "Datadeling".
+4. Klik på "Opret Token".
+5. Navngiv dit token og klik på "Opret Token".
+6. Kopier dit token og noter det et sikkert sted. Du ser det kun én gang.
+7. Du kan nu bruge token her i app'en.
 
 ## Datakilder
 Dette værktøj trækker på data fra flere kilder både til udtræk af personligt forbrug, men også til udtræk af tariffer og spotpriser.
